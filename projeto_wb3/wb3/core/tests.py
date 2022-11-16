c608c265bcaad28e0fea5602cce6a2ca48eba190
Guilherme Afonso <86564645+ParaQueNome@users.noreply.github.com>
2022-11-16 20:32:46 -0300

    Adicionando Palestras no templates.html

--- projeto_wb3/wb3/core/tests.py
+++ projeto_wb3/wb3/core/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from http import HTTPStatus
 
 # Create your tests here.
+class SemPalestra(TestCase):
+    def test_200_response(self):
+        if resp.status == HTTPStatus.Ok:
+            self.assertEqual('Ok', self.resp.status_code)
