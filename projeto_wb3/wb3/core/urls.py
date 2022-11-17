a9cb09bda9f163949b2475de2433f45b288b5f2a
ParaQueNome <guiaugusto455@gmail.com>
2022-11-16 22:04:42 -0300

    Adicionando uma lista de palestras e devolvendo um HttpResponse com contexto

--- projeto_wb3/wb3/core/urls.py
+++ projeto_wb3/wb3/core/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('',views.cadastrar),
+    path('',views.palestra),
     
 
 ]
