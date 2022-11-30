127966cdbcc402bd3700ae3425fcb3d5b4a7833b
ParaQueNome <guiaugusto455@gmail.com>
2022-11-30 19:24:20 -0300

    Adicionando lista de palestras

--- projeto_wb3/wb3/core/views.py
+++ projeto_wb3/wb3/core/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def palestra(request):
-    return render(request, 'templates.html')
-    
+    palestra = ['Marketing Digital','Design Digital','Programação Web','Programação Orientada a Objetos','Pentest','Mineração de Dados','Lógica e Algoritmos para programação']    
+    contexto = {'Palestras': palestra}
+    return render(request,'templates.html',contexto)
