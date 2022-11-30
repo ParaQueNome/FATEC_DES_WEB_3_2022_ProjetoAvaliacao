7201e81838c34e25aa492e7911131d6982a8ce6c
ParaQueNome <guiaugusto455@gmail.com>
2022-11-30 19:40:05 -0300

    Core.views modified

--- projeto_wb3/wb3/core/views.py
+++ projeto_wb3/wb3/core/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 def palestra(request):
     palestra = ['Marketing Digital','Design Digital','Programação Web','Programação Orientada a Objetos','Pentest','Mineração de Dados','Lógica e Algoritmos para programação']    
     contexto = {'Palestras': palestra}
-    return render(request,'templates.html',contexto)
+    if len(palestra) > 0:
+
+        return render(request,'templates.html',contexto)
+    else:
+        contexto = {'Palestras': False}
+        return render(request, 'templates.html',contexto)
+        
