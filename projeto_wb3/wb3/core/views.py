7788765292fe212f403406b950f10f11cdd1601c
ParaQueNome <guiaugusto455@gmail.com>
2022-11-30 20:21:29 -0300

    Corrigindo erro do HTML e listando as palestras

--- projeto_wb3/wb3/core/views.py
+++ projeto_wb3/wb3/core/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def palestra(request):
     palestra = [['Marketing Digital','Carlos','Lab01','26/11'],['Design Digital','Mariana','Lab03','26/11'],['Programação Web', 'Jonas','Lab01','27/11'],['Programação Orientada a Objetos','Jonas','Lab01','27/11'],['Pentest','Silmara','Lab03','27/11'],['Mineração de Dados','Amanda','Lab03','28/11'],['Lógica e Algoritmos para programação','Carlos','Lab01','28/11']]    
-    contexto = {'Palestras': palestra}
+    contexto = {'Palestras': True, 'palestra': palestra}
     if len(palestra) > 0:
 
         return render(request,'templates.html',contexto)
     else:
         contexto = {'Palestras': False}
         return render(request, 'templates.html',contexto)
+
+
+'''def cadastrar(request):'''
